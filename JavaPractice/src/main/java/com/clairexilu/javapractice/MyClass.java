f974c356b2ae1bda82e6c0c7bc3174ade5c9f3bc
Claire Lu <appsfunlu@gmail.com>
2021-11-21 20:06:24 -0800

    initial setup

---
+++ JavaPractice/src/main/java/com/clairexilu/javapractice/MyClass.java
@@ -0,0 +1,13 @@
+package com.clairexilu.javapractice;
+
+public class MyClass {
+
+        public static void main(String[] args) {
+            System.out.println("Hello World");
+            System.out.println("Hello Again");
+
+            String claire;
+            claire = "Claire Lu";
+            System.out.println(claire);
+        }
+}
